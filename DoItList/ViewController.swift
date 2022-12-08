eecdcb713d1f3c18cc8fedf4228457f4b264d1e3
gdking137 <gdkim137@gmail.com>
2022-12-08 17:10:15 -0500

    Initial Commit

---
+++ DoItList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoItList
+//
+//  Created by Songhee Yim on 12/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
